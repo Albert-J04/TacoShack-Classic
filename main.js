a1208435316c602f790dfb7cb16315b27e16c148
Albert-J04 <45517745+Albert-J04@users.noreply.github.com>
2021-01-12 00:38:39 +0000

    fix hourly income probably

--- main.js
+++ main.js
@@ -6,30 +6,38 @@ const shacks = require("./schemas/shacks.js");
 const requireAll = require('require-all');
 const path = require('path');
 const fs = require('fs');
+const cron = require('cron')
+
+bot.logWebhook = new Discord.WebhookClient('VALUE', 'VALUE')
+
+let hourlyIncome = new cron.CronJob('0 * * * *', () => {
+	count = 0
+	var d = new Date().toLocaleTimeString();
+	bot.logWebhook.send(`[\`${d}\`] Hourly Income Starting!`);
+	shacks.find()
+	.then(async (results) => {
+		results.forEach(result => {
+			count = count + 1
+			shacks.findOne({userID: result.userID}, async (err, data) => {
+				if (err || !data) return console.log('weird')
+				var tacorando = Math.floor(Math.random() * (20 - 5) ) + 5;
+				var tacos = Math.floor((Math.round(data.income / 4)) + tacorando);
+				data.balance = data.balance + data.income;
+				data.tacos = data.tacos + tacos;
+				if (count === results.length){
+					var d = new Date().toLocaleTimeString();
+					return bot.logWebhook.send(`[\`${d}\`] Hourly Income Done!`);
+				}
+			})
+		})
+	});
+})
 
 //ready
 bot.on('ready', () => {
     console.log("Taco Shack Ready to Sell Some Tacos!")
-    bot.user.setActivity(`with v1.0`)
-
-    // HOURLY INCOME
-    setInterval (function () {
-		shacks.find()
-		.then(async (results) => {
-			results.forEach(result => {
-				shacks.findOne({userID: results.userID}, async (err, data) => {
-					if (err || !data) return console.log('weird')
-					var tacorando = Math.floor(Math.random() * (20 - 5) ) + 5;
-					var tacos = Math.floor((Math.round(shacks[key].income / 4)) + tacorando);
-					data.balance = data.balance + data.income;
-					data.tacos = data.tacos + tacos;
-				})
-			})
-		})
-        return bot.channels.cache.get('787401447217954816').send(`Hourly Income Sent!`);
-
-    }, 3600000);//3600000
-
+	bot.user.setActivity(`with v1.0`)
+	hourlyIncome.start()
 });
 
 bot.on("error", (e) => console.error("Error " + e));
