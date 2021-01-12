8bcec964ef1444bd047382b4d55635c86449e0fc
Albert-J04 <45517745+Albert-J04@users.noreply.github.com>
2021-01-12 21:39:31 +0000

    Update functions.js

--- util/functions.js
+++ util/functions.js
@@ -28,7 +28,7 @@ module.exports = {
         var dt2 = new Date(created);
         const diffDays = Math.floor((Date.UTC(dt2.getFullYear(), dt2.getMonth(), dt2.getDate()) - Date.UTC(dt1.getFullYear(), dt1.getMonth(), dt1.getDate()) ) /(1000 * 60 * 60 * 24));
         return new Discord.MessageEmbed()
-        .setColor("#99AAB5")
+        .setColor("ORANGE")
         .setAuthor('Command does not exist yet!')
         .setDescription(`📆 That would not be a command for another **${diffDays}** days!`);
     },
