1cc4ddff710571218eab83ae80245cb0130f304f
Albert-J04 <45517745+Albert-J04@users.noreply.github.com>
2021-01-18 13:30:28 +0000

    update hourly to use an array of embeds for logging

--- hourlyIncome.js
+++ hourlyIncome.js
@@ -1,7 +1,9 @@
 const settings = require('./util/settings.json')
 const shacks = require("./schemas/shacks.js");
+const Discord = require('discord.js');
 module.exports = {
     send: (bot) => {
+        out = []
         var d = new Date().toLocaleTimeString();
         bot.logWebhook.send(`⏱ [\`${d}\`] Hourly Income Starting!`);
         shacks.find()
@@ -22,9 +24,25 @@ module.exports = {
                     data.balance = data.balance + data.income;
                     data.tacos = data.tacos + tacos;
                     data.save().catch(err => console.log(err))
-                    bot.logWebhook.send(`** **\n**${data.name}** (\`${data.userID}\`)\n**Previous:** Bal: \`${oldBalance}\` | Tacos: \`${oldTacos}\`\n**+After:** Bal: \`${data.balance}\` | Tacos: \`${data.tacos}\``);
+                    out.push(`**${data.name}** (\`${data.userID}\`)\n**Before:** Bal: \`${oldBalance}\` | Tacos: \`${oldTacos}\`\n**+After:** Bal: \`${data.balance}\` | Tacos: \`${data.tacos}\``);
                     i++;
                     if (i === results.length){
+                        var string = out.join('\n━━━━━━━━━━━━━━\n')
+                        var parts = string.split(/(.{2048})/).filter(O=>O)
+                        embeds = []
+                        parts.forEach(async part => {
+                            const embed = new Discord.MessageEmbed()
+                            embed.setDescription(part)
+                            embed.setColor(`BLURPLE`)
+                            embeds.push(embed)
+                        })
+                        if (embeds.length > 10){
+                            embeds.forEach(embed => {
+                                bot.logWebhook.send(embed)
+                            })
+                        }else{
+                            bot.logWebhook.send({embeds})
+                        }
                         var d = new Date().toLocaleTimeString();
                         return bot.logWebhook.send(`✅ [\`${d}\`] Hourly Income Done!`);
                     }
