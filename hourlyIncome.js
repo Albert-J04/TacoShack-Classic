6f128dfe07e3c1785f4c5ae55a72acd73a906caf
Albert-J04 <45517745+Albert-J04@users.noreply.github.com>
2021-01-18 23:01:34 +0000

    Update hourlyIncome.js

--- hourlyIncome.js
+++ hourlyIncome.js
@@ -17,33 +17,14 @@ module.exports = {
                     if (!data) {
                         return bot.logWebhook.send(`⚠ Lack of data!`);
                     }
-                    const oldBalance = data.balance
-                    const oldTacos = data.tacos
                     var tacorando = Math.floor(Math.random() * (20 - 5) ) + 5;
                     var tacos = Math.floor((Math.round(data.income / 4)) + tacorando);
                     data.balance = data.balance + data.income;
                     data.tacos = data.tacos + tacos;
                     data.save().catch(err => console.log(err))
-                    out.push(`\`${i + 1}\` **${data.name}** (\`${data.userID}\`)\n**Before:** Bal: \`${oldBalance}\` | Tacos: \`${oldTacos}\`\n**+After:** Bal: \`${data.balance}\` | Tacos: \`${data.tacos}\`\nHourly: \`${data.income}\``);
                     i++;
                     if (i === results.length){
-                        setTimeout(function() {
-                            var string = out.join('\n━━━━━━━━━━━━━━\n')
-                            var parts = string.split(/(.{1500})/).filter(O=>O)
-                            embeds = []
-                            parts.forEach(async part => {
-                                const embed = new Discord.MessageEmbed()
-                                    .setDescription(part)
-                                    .setColor(`BLURPLE`)
-                                embeds.push(embed)
-                            })
-                            const split = chunkArray(embeds, 10)
-                            split.forEach(async chunk => {
-                                await bot.logWebhook.send({embeds: chunk})
-                            })
-                            var d = new Date().toLocaleTimeString();
-                            return bot.logWebhook.send(`✅ [\`${d}\`] Hourly Income Done!`);
-                        }, 500) // <-- timeout in MS
+                        return bot.logWebhook.send(`✅ [\`${d}\`] Hourly Income Done!`);
                     }
                 })
             })
@@ -51,18 +32,4 @@ module.exports = {
 
       
     }
-}
-
-
-function chunkArray(myArray, chunk_size){
-    var index = 0;
-    var arrayLength = myArray.length;
-    var tempArray = [];
-    
-    for (index = 0; index < arrayLength; index += chunk_size) {
-        myChunk = myArray.slice(index, index+chunk_size);
-        tempArray.push(myChunk);
-    }
-
-    return tempArray;
 }
