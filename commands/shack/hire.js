8b3a80c3446d4923c042630ff82f6e31ffd67316
Albert-J04 <45517745+Albert-J04@users.noreply.github.com>
2021-01-19 08:47:44 +0000

    bruh cole make dynamic code

--- commands/shack/hire.js
+++ commands/shack/hire.js
@@ -33,31 +33,31 @@ if(!args[0]) {
         .setColor('0x3477e2')
         .setThumbnail('https://cdn.dribbble.com/users/72556/screenshots/1711901/8bit-taco.jpg')
         .setDescription(`\n
-**Apprentice Chef**  \`(${data.employees[231]}/5)\`
+**Apprentice Chef**  \`(${data.employees[231]}/15)\`
 Cost: $${costCalc(upgrades[231].price, data.employees[231])}
 Boost: +$${upgrades[231].boost}/hr
 ID: 231\n
-**Cook**  \`(${data.employees[232]}/5)\`
+**Cook**  \`(${data.employees[232]}/15)\`
 Cost: $${costCalc(upgrades[232].price, data.employees[232])}
 Boost: +${upgrades[232].boost}/hr
 ID: 232\n
-**Sous Chef**  \`(${data.employees[233]}/3)\`
+**Sous Chef**  \`(${data.employees[233]}/13)\`
 Cost: $${costCalc(upgrades[233].price, data.employees[233])}
 Boost: +${upgrades[233].boost}/hr
 ID: 233\n
-**Head Chef**  \`(${data.employees[234]}/3)\`
+**Head Chef**  \`(${data.employees[234]}/13)\`
 Cost: $${costCalc(upgrades[234].price, data.employees[234])}
 Boost: +${upgrades[234].boost}/hr
 ID: 234\n
-**Executive Chef**  \`(${data.employees[235]}/2)\`
+**Executive Chef**  \`(${data.employees[235]}/12)\`
 Cost: $${costCalc(upgrades[235].price, data.employees[235])}
 Boost: +${upgrades[235].boost}/hr
 ID: 235\n
-**Advertiser**  \`(${data.employees[236]}/3)\`
+**Advertiser**  \`(${data.employees[236]}/13)\`
 Cost: $${costCalc(upgrades[236].price, data.employees[236])}
 Boost: +${upgrades[236].boost}/hr
 ID: 236\n
-**Greeter**  \`(${data.employees[237]}/2)\`
+**Greeter**  \`(${data.employees[237]}/12)\`
 Cost: $${costCalc(upgrades[237].price, data.employees[237])}
 Boost: +${upgrades[237].boost}/hr
 ID: 237\n
--- commands/shack/upgrades.js
+++ commands/shack/upgrades.js
@@ -29,27 +29,27 @@ module.exports.run = async (bot, message, args) => {
         .setColor('0x1ced31')
         .setThumbnail('https://cdn.dribbble.com/users/72556/screenshots/1711901/8bit-taco.jpg')
         .setDescription(`\n
-**New Paint**  \`(${data.upgrades[121]}/3)\`
+**New Paint**  \`(${data.upgrades[121]}/13)\`
 Cost: $${costCalc(upgrades[121].price, data.upgrades[121])}
 Boost: +$${upgrades[121].boost}/hr
 ID: 121\n
-**New Furniture**  \`(${data.upgrades[122]}/3)\`
+**New Furniture**  \`(${data.upgrades[122]}/13)\`
 Cost: $${costCalc(upgrades[122].price, data.upgrades[122])}
 Boost: +$${upgrades[122].boost}/hr
 ID: 122\n
-**Better Appliances**  \`(${data.upgrades[123]}/5)\`
+**Better Appliances**  \`(${data.upgrades[123]}/15)\`
 Cost: $${costCalc(upgrades[123].price, data.upgrades[123])}
 Boost: +$${upgrades[123].boost}/hr
 ID: 123\n
-**Nicer Batchrooms**  \`(${data.upgrades[124]}/2)\`
+**Nicer Batchrooms**  \`(${data.upgrades[124]}/12)\`
 Cost: $${costCalc(upgrades[124].price, data.upgrades[124])}
 Boost: +$${upgrades[124].boost}/hr
 ID: 124\n
-**Billboard**  \`(${data.upgrades[125]}/5)\`
+**Billboard**  \`(${data.upgrades[125]}/15)\`
 Cost: $${costCalc(upgrades[125].price, data.upgrades[125])}
 Boost: +$${upgrades[125].boost}/hr
 ID: 125\n
-**Cooler Tip Jar**  \`(${data.upgrades[126]}/3)\`
+**Cooler Tip Jar**  \`(${data.upgrades[126]}/13)\`
 Cost: $${costCalc(upgrades[126].price, data.upgrades[126])}
 Boost: +$${upgrades[126].boost}/hr
 ID: 126\n
